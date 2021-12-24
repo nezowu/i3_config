7b3aa72bb9246067718fb6a0ad6532784875b7f6
nezabudka <gxendz@gmail.com>
2021-12-24 16:54:12 +0300

    serial

--- serial_console.sh
+++ serial_console.sh
@@ -1,6 +1,7 @@
 #/etc/profile.d/serial_console.sh 
-if [ "$TERM" = vt220 ]; then
+#if [ "$TERM" = vt220 ]; then
     #stty rows 35 cols 127
     stty -F /dev/ttyS0 cols 127 
     stty -F /dev/ttyS0 rows 35
-fi
+#fi
+#stty назначает размер именно vt220!
