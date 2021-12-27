1e5c774d38260da3886d96311000a61f42d1ecc4
nezabudka <gxendz@gmail.com>
2021-12-28 00:35:02 +0300

    serial

--- serial_console.sh
+++ serial_console.sh
@@ -1,7 +1,10 @@
 #/etc/profile.d/serial_console.sh 
 #if [ "$TERM" = vt220 ]; then
     #stty rows 35 cols 127
-    stty -F /dev/ttyS0 cols 127 
-    stty -F /dev/ttyS0 rows 35
+    stty -F /dev/ttyS0 cols 127 rows 35
 #fi
 #stty назначает размер именно vt220!
+#/etc/default/grub
+#GRUB_CMDLINE_LINUX="console=tty0 console=ttyS0,115200"
+#GRUB_TERMINAL=serial
+#update-grub
