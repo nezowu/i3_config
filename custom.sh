09525c174e3d7660ac94dc31a04f96cd5e4332e0
neza <zowu@list.ru>
2023-02-04 16:36:25 +0300

    profile_d

--- custom.sh
+++ custom.sh
@@ -1,7 +1,7 @@
 #chmod 644 /etc/profile.d/custom.sh
 if [[ $(tty) =~ "/dev/tty" ]]; then
         if [ "$PS1" ]; then
-                PS1="[\A@\l \W]\\$ "
+                PS1='[\A@\l \W]\$ '
 		setterm --blank 0 #отмена выключения экрана
         fi
 fi
--- ranger.sh
+++ ranger.sh
@@ -1,8 +1,6 @@
 #chmod 644 /etc/profile.d/ranger.sh
 if [ $(ps -p $PPID -o comm=) = "ranger" ]; then
-	if [ $(id -u) -eq 0 ]; then
-		PS1="[\u@\h \W]#> "
-	else
-		PS1="[\u@\h \W]\\$> "
-	fi
+		PS1='[\u@\h \W]\$> '
 fi
+# \$ - if the effective UID is 0, a #, otherwise a $
+# only with single quotes
